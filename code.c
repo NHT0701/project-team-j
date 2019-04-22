d9b5e768fb2738f0ff9fc1b2c47220d07886e15a
ShreyasGoyal <49230799+ShreyasGoyal@users.noreply.github.com>
2019-04-22 21:40:13 +0800

    Update code.c
    Just made one change. Just to polish the code a little. Please review

--- code.c
+++ code.c
@@ -5,7 +5,7 @@ int main(){
     int i, n = 10, t1 = 0, t2 = 1, nextTerm;
     for (i = 1; i <= n; ++i) //runs iteration from 1 to 10.
     {
-        printf("%d, ", t1);  //prints 0 (first number)
+        printf("%d ", t1);  //prints 0 (first number)
         nextTerm = t1 + t2;  //prints 1 (second number)
         t1 = t2;             //t1 becomes the next number (t2)
         t2 = nextTerm;       //t2 becomes the nextTerm.
